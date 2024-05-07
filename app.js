7f67049bc1143fef6630a51cc42640db60377d4e
Suhyun Kim <kim00431@algonquinlive.com>
2024-05-06 22:25:23 -0400

    express install

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("index")
+
+/*
+const http = require('http');
+const app = http.createServer( (req,res) => {  
+    res.end('HELLO')
+});
+*/
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+/* get, post, put, delete */
+app.get('/', (req,res) => {
+    res.send('Home page')
+})
+app.get('/gallery', (req,res) => {
+    res.send('Welcome to gallery')
+})
+app.listen(PORT,() => console.log(`${PORT} portserver is running`) ) 
+
